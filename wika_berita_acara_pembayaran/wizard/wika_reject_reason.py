72509929f45423b10f668b350cdb45528f9a24f6
firgysciglio@gmail.com <firgysciglio@gmail.com>
2023-12-29 09:01:57 +0700

    create modul wika berita acara pembayaran

---
+++ wika_berita_acara_pembayaran/wizard/wika_reject_reason.py
@@ -0,0 +1,24 @@
+from odoo import models, fields
+from datetime import datetime, timedelta
+
+class RejectWizard(models.TransientModel):
+    _name = 'reject.wizard'
+    _description = 'Reject Wizard'
+
+    reject_reason = fields.Text(string='Reject Reason')
+
+    def cancel(self):
+        return
+    
+    def ok(self):
+        active_id = self.env.context.get('active_id')
+        # groups_id = self._context['groups_id']
+        if active_id:
+            audit_log_obj = self.env['wika.bap.approval.line'].create({'user_id': self._uid,
+                # 'groups_id': groups_id,
+                'datetime': datetime.now(),
+                'note': "Reject ( " + self.reject_reason + " )",
+                'bap_id': active_id,
+                })
+            return {'type': 'ir.actions.act_window_close'}
+        
---
+++ wika_project/models/wika_berita_acara_pembayaran.py
@@ -0,0 +1,7 @@
+from odoo import models, fields, api
+
+
+class WikaBeritaAcaraPembayaran(models.Model):
+    _name = 'wika.berita.acara.pembayaran'
+    _decription = 'wika berita acara pembayaran module'
+
