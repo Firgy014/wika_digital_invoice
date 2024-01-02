d1d106f953b1f3c5e119d4bd49b8ca69e615684e
Susi <vsusii9@gmail.com>
2024-01-02 14:11:42 +0700

    Split addons matrix approval dan purchase

---
+++ wika_purchase/__manifest__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Inherit Purchase",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','purchase','wika_matrix_approval'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/purchase_views.xml',
+        'views/wizard.xml',
+        'views/action.xml',
+        'views/menu.xml',
+        'wizard/wika_reject_reason.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
--- wika_purchase/models/purchase.py
+++ wika_purchase/models/purchase.py
@@ -37,72 +37,7 @@ class PurchaseOrderInherit(models.Model):
     sap_doc_number = fields.Char(string='SAP Doc Number')
     step_approve = fields.Integer(string='Step Approve')
 
-    def _get_matrix_approval_group(self):
-        #     model_id = self.env['ir.model'].sudo().search([('name', '=', 'matrix.approval')], limit=1)
-        #     group_id = self.env['res.groups'].sudo().search([
-        #         ('model_access.model_id', '=', model_id.id)
-        #     ])
-        #     if group_id:
-        #         return group_id
-        #     else:
-        #         return False
-        return None
 
-    @api.onchange('partner_id')
-    def _onchange_visibility(self):
-        matrix_approval_group = self._get_matrix_approval_group()
-
-        if matrix_approval_group:
-            print("User has access to confirm a purchase order based on the matrix_approval_group")
-        else:
-            print("User has no access to confirm a purchase order based on the matrix_approval_group")
-
-    def button_confirm(self):
-        res = super(PurchaseOrderInherit, self).button_confirm()
-        matrix_approval_group = self._get_matrix_approval_group()
-        po_approval_model = self.env['wika.purchase.order.approval.history'].sudo()
-
-        if matrix_approval_group:
-            po_approval_model.create({
-                'purchase_order_id': self.id,
-                'user_id': self.env.user.id,
-                'group_id': matrix_approval_group.id,
-                'date': datetime.now(),
-                'note': f'Approved by {self.env.user.name} at {datetime.now().strftime("%d-%m-%Y")} for unconditional reason.'
-            })
-            return res
-        else:
-            return {
-                'name': _('Access Denied'),
-                'type': 'ir.actions.act_window',
-                'view_mode': 'form',
-                'view_id': self.env.ref('wika_matrix_approval.wizard_simple_form').id,
-                'view_type': 'form',
-                'res_model': 'ir.actions.act_window',
-                'target': 'new',
-                'context': {
-                    'default_message': _('You do not have the required access to confirm the Purchase Order.'),
-                },
-            }
-
-    def button_cancel(self):
-        res = super(PurchaseOrderInherit, self).button_cancel()
-        matrix_approval_group = self._get_matrix_approval_group()
-        po_approval_model = self.env['wika.purchase.order.approval.history'].sudo()
-
-        if matrix_approval_group:
-            print("User has access to cancel a purchase order based on the matrix_approval_group")
-            po_approval_model.create({
-                'purchase_order_id': self.id,
-                'user_id': self.env.user.id,
-                'group_id': matrix_approval_group.id,
-                'date': datetime.now(),
-                'note': f'Canceled by {self.env.user.name} at {datetime.now().strftime("%d-%m-%Y")} for unconditional reason.'
-            })
-            return res
-        else:
-            print("User has no access to cancel a purchase order based on the matrix_approval_group")
-            return {'type': 'ir.actions.act_window_close'}
 
     @api.model_create_multi
     def create(self, vals_list):
@@ -133,25 +68,6 @@ class PurchaseOrderInherit(models.Model):
             else:
                 raise AccessError(
                     "Either approval and/or document settings are not found. Please configure it first in the settings menu.")
-
-            # for group in res.env.user.groups_id:
-            #     for menu in group.menu_access:
-            #         if 'Purchase' in menu.name:
-            #             if len(menu.parent_path) > 4:
-            #                 po_approval_group = group
-
-            # if appr_setting_id:
-            #     for approval_line in appr_setting_id:
-            #         approval_list.append((0,0, {
-            #             'purchase_id': res.id,
-            #             'user_id': res.env.user.id,
-            #             'groups_id': po_approval_group.id,
-            #             'date': datetime.now(),
-            #             'note': f'Approved by {res.env.user.name} while creating a Purchase Order with reference: {res.name} at {datetime.now(jkt_tz).strftime("%d-%m-%Y")}.'
-            #         }))
-            #     res.history_approval_ids = approval_list
-            # else:
-            #     raise AccessError("Either approval and/or document settings are not found. Please configure it first in the settings menu.")
 
         return res
 
@@ -187,19 +103,9 @@ class PurchaseOrderInherit(models.Model):
         else:
             raise ValidationError('User Akses Anda tidak berhak Approve!')
 
-    # def action_reject(self):
-    #     action = self.env.ref('wika_matrix_approval.action_reject_wizard').read()[0]
-    #     return action
 
     def action_reject(self):
-        # stage_sekarang = self.stage_id
-        # if self.stage_id.name == 'Proses Approval':
-        #
-        #     groups_line = self.env['loan.stage.line'].search([('stage_id', '=', stage_sekarang.id),('sequence','=',self.step_approve)],limit=1)
-        # else:
-        #     groups_line = self.env['loan.stage.line'].search(
-        #         [('stage_id', '=', stage_sekarang.id), ('sequence', '=', self.step_approve-1)], limit=1)
-        # groups_id  = self.env['res.groups'].search([('id','=',groups_line.groups_id.id)])
+
         user = self.env['res.users'].search([('id', '=', self._uid)], limit=1)
         cek = False
         model_id = self.env['ir.model'].search([('model', '=', 'purchase.order')], limit=1)
@@ -237,6 +143,7 @@ class PurchaseOrderInherit(models.Model):
 
 class PurchaseOrderDocumentLine(models.Model):
     _name = 'wika.po.document.line'
+    _description = 'List Document PO'
 
     purchase_id = fields.Many2one('purchase.order', string='Purchase')
     document_id = fields.Many2one('wika.document.setting', string='Document')
@@ -251,27 +158,10 @@ class PurchaseOrderDocumentLine(models.Model):
 
 class PurchaseOrderApprovalLine(models.Model):
     _name = 'wika.po.approval.line'
+    _description = 'History Approval PO'
 
     purchase_id = fields.Many2one('purchase.order', string='Purchase')
     user_id = fields.Many2one('res.users', string='User')
     groups_id = fields.Many2one('res.groups', string='Groups')
     date = fields.Datetime(string='Date')
     note = fields.Char(string='Note')
-# -*- coding: utf-8 -*-
-
-# from odoo import models, fields, api
-
-
-# class wika_purchase(models.Model):
-#     _name = 'wika_purchase.wika_purchase'
-#     _description = 'wika_purchase.wika_purchase'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
