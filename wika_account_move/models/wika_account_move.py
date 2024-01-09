1b2e309545e3b055babf27cf9c8fed3aa78f2c82
firgysciglio@gmail.com <firgysciglio@gmail.com>
2024-01-09 10:09:39 +0700

    add function create activity and domain in module wika_account_move, wika_berita_acara_pembayaran and wika_payment request

--- wika_account_move/models/wika_account_move.py
+++ wika_account_move/models/wika_account_move.py
@@ -9,7 +9,6 @@ class WikaInheritedAccountMove(models.Model):
     branch_id = fields.Many2one('res.branch', string='Divisi')
     department_id = fields.Many2one('res.branch', string='Department')
     project_id = fields.Many2one('project.project', string='Project')
-    pr_id = fields.Many2one('wika.payment.request', string='Payment Request')
     document_ids = fields.One2many('wika.invoice.document.line', 'invoice_id', string='Document Line')
     history_approval_ids = fields.One2many('wika.invoice.approval.line', 'invoice_id', string='History Approval Line')
     reject_reason_account = fields.Text(string='Reject Reason')
@@ -18,7 +17,7 @@ class WikaInheritedAccountMove(models.Model):
         selection=[
             ('draft', 'Draft'),
             ('upload', 'Upload'),
-            ('request', 'Request'),
+            ('approve', 'Approve'),
             ('reject', 'Reject'),
             ('posted', 'Posted'),
             ('cancel', 'Cancelled'),
@@ -32,14 +31,36 @@ class WikaInheritedAccountMove(models.Model):
         default='draft',
     )
 
+    message_follower_ids = fields.One2many(
+        'mail.followers', 'res_id', string='Followers', groups='base.group_user')
+    activity_ids = fields.One2many(
+        'mail.activity', 'res_id', 'Activities',
+        auto_join=True,
+        groups="base.group_user",)
+
     def action_submit(self):
         self.write({'state': 'upload'})
         self.step_approve += 1
-
-    def action_request(self):
-        self.write({'state': 'request'})
+        model_id = self.env['ir.model'].search([('model', '=', 'account.move')], limit=1)
+        model_wika_id = self.env['wika.approval.setting'].search([('model_id', '=', model_id.id)], limit=1)
+        user = self.env['res.users'].search([('branch_id', '=', self.branch_id.id)])
+        if model_wika_id:
+            groups_line = self.env['wika.approval.setting.line'].search([
+                ('branch_id', '=', self.branch_id.id),
+                ('sequence', '=', self.step_approve),
+                ('approval_id', '=', model_wika_id.id)
+            ], limit=1)
+            groups_id = groups_line.groups_id
+        for x in groups_id.users:
+            activity_ids = self.env['mail.activity'].create({
+                    'activity_type_id': 4,
+                    'res_model_id': self.env['ir.model'].sudo().search([('model', '=', 'account.move')], limit=1).id,
+                    'res_id': self.id,
+                    'user_id': x.id,
+                    'summary': """ Need Approval Document PO """
+                })
     
-    def action_post(self):
+    def action_approve(self):
         user = self.env['res.users'].search([('id','=',self._uid)], limit=1)
         print("TESTTTTTTTTTTTTTT", user)
         cek = False
@@ -55,10 +76,10 @@ class WikaInheritedAccountMove(models.Model):
             for x in groups_id.users:
                 if x.id == self._uid:
                     cek = True
-
+        
         if cek == True:
             if model_wika_id.total_approve == self.step_approve:
-                self.state = 'approved'
+                self.state = 'approve'
             else:
                 self.step_approve += 1
 
@@ -101,6 +122,28 @@ class WikaInheritedAccountMove(models.Model):
         else:
             raise ValidationError('User Akses Anda tidak berhak Reject!')
 
+    @api.onchange('partner_id')
+    def _onchange_(self):
+        if self.partner_id:
+            model_model = self.env['ir.model'].sudo()
+            document_setting_model = self.env['wika.document.setting'].sudo()
+            model_id = model_model.search([('model', '=', 'account.move')], limit=1)
+            print("partner_id----------TEST--------", model_id)
+            doc_setting_id = document_setting_model.search([('model_id', '=', model_id.id)])
+
+            if doc_setting_id:
+                self.document_ids.unlink()
+
+                document_list = []
+                for document_line in doc_setting_id:
+                    document_list.append((0, 0, {
+                        'invoice_id': self.id,
+                        'document_id': document_line.id,
+                        'state': 'waiting'
+                    }))
+                self.document_ids = document_list
+            else:
+                raise AccessError("Data dokumen tidak ada!")
 
 class WikaInvoiceDocumentLine(models.Model):
     _name = 'wika.invoice.document.line'
