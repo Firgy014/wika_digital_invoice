f197ef0c7d24d01cd1802387dc8ce8d5f83d43f9
Susi <vsusii9@gmail.com>
2024-01-15 16:01:30 +0700

    API PO,GR

--- wika_integration/models/res_config.py
+++ wika_integration/models/res_config.py
@@ -35,40 +35,86 @@ class ResConfigSettings(models.TransientModel):
             raise UserError(_("Connection Failed. Please Check Your Internet Connection."))
         if txt['data']:
             txt_data = txt['data']
-            print (txt_data)
-            for hasil in txt_data:
-                print (hasil)
-                # project  = self.env['project.project'].sudo().search([
-                #     ('sap_code', '=' ,hasil['PRCTR']), ('company_id', '=', 1)] ,limit=1)
-                # if project:
-                #     profit_center=project.id
-                #     branch_id = project.branch_id.id
-                #     department_id = None
-                # if not project:
-                #     branch = self.env['res.branch'].sudo().search([
-                #         ('sap_code', '=', hasil['PRCTR']), ('company_id', '=', 1)], limit=1)
-                #     if branch and branch.biro == True:
-                #         department_id= branch.id
-                #         branch_id=branch.parent_id.id
-                #     if branch and branch.biro == False:
-                #         department_id = None
-                #         branch_id = branch.id
-                #     if not branch:
-                #         return "Kode Profit Center : %s tidak ditemukan" % hasil['PRCTR']
-                #
-                # po_create= self.env['purchase.order'].sudo().create({
-                #     'name': hasil['EBELN'],
-                #
-                # })
-        #         # else:
-        #         #     account = self.env['account.account'].sudo().create({
-        #         #         'code': kode_coa_sap[:-3],
-        #         #         'name': nama_coa,
-        #         #         'sap_code': kode_coa_sap,
-        #         #         'company_id' :1,
-        #         #         'user_type_id' :9
-        #         #     })
-        #
+
+            for data in txt_data:
+                vals = []
+                print (data)
+                isi = data['isi']
+                for hasil in isi:
+                    if data['jenis']=='JASA':
+                        prod = self.env['product.product'].sudo().search([
+                            ('default_code', '=', hasil['SRVPOS'])], limit=1)
+                        price= float(hasil['BRTWR'])
+                        qty=float(hasil['MENGE'])
+                        price_unit= float(price/qty)
+                    else:
+                        prod = self.env['product.product'].sudo().search([
+                            ('default_code', '=', hasil['MATNR'])], limit=1)
+                        price = float(hasil['NETPR'])
+                        qty = float(hasil['MENGE'])
+                        price_unit= float(price/qty)
+                    if not prod:
+                        return "Material/Service  : %s tidak ditemukan" % (hasil['SRVPOS'],hasil['MATNR'])
+                    tax= self.env['account.tax'].sudo().search([
+                        ('name', '=', hasil['MWSKZ'])], limit=1)
+                    if not tax:
+                        return "Kode Pajak  : %s tidak ditemukan" % hasil['MWSKZ']
+                    curr=self.env['account.currency'].sudo().search([
+                        ('name', '=', hasil['WAERS'])], limit=1)
+                    if not curr:
+                        return "Kode Currency  : %s tidak ditemukan" % hasil['WAERS']
+                    vendor = self.env['res.partner'].sudo().search([
+                        ('sap_code', '=', hasil['LIFNR'])], limit=1)
+                    if not vendor:
+                        return "Vendor  : %s tidak ditemukan" % hasil['LIFNR']
+
+                    print ("vendor",vendor)
+                    uom= self.env['uom.uom'].sudo().search([
+                        ('name', '=' ,hasil['MEINS'])] ,limit=1)
+                    if not uom:
+                        uom = self.env['uom.uom'].sudo().create({
+                            'name': hasil['MEINS'],'category_id':1})
+                    project  = self.env['project.project'].sudo().search([
+                        ('sap_code', '=' ,data['PRCTR']), ('company_id', '=', 1)] ,limit=1)
+                    if project:
+                        profit_center=project.id
+                        branch_id = project.branch_id.id
+                        department_id = None
+                    if not project:
+                        branch = self.env['res.branch'].sudo().search([
+                            ('sap_code', '=', data['PRCTR']), ('company_id', '=', 1)], limit=1)
+                        if branch and branch.biro == True:
+                            department_id= branch.id
+                            branch_id=branch.parent_id.id
+                            profit_center=None
+                        if branch and branch.biro == False:
+                            department_id = None
+                            branch_id = branch.id
+                            profit_center = None
+                        if not branch:
+                            return "Kode Profit Center : %s tidak ditemukan" % data['PRCTR']
+                    vals.append((0,0, {
+                        'product_id': prod.id if prod else False,
+                        'product_qty':qty,
+                        'product_uom':uom.id,
+                        'price_unit':price_unit,
+                        'taxes_id':[(6, 0, [x.id for x in tax])]
+
+                    }))
+                po_create= self.env['purchase.order'].sudo().create({
+                    'name': data['EBELN'],
+                    'partner_id': vendor.id if vendor else False,
+                    'project_id':profit_center,
+                    'branch_id':branch_id,
+                    'department_id':department_id,
+                    'order_line':vals,
+                    'currency_id': curr.id,
+                    'begin_date':data['BEDAT_X'],
+                    'vendor_ref':data['IDNLF'],
+                    'po_type':data['BSART'],
+                    'state':'po'
+                })
+
         else:
             raise UserError(_("Data PO Tidak Tersedia!"))
 
