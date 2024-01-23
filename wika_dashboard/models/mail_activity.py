3c38936b40f87676123e2d40a19f8cb6485e07f5
andromedasupendi <andromedasupendi@gmail.com>
2024-01-23 16:10:19 +0700

    fix activity creation, add menu in activity list, add pdf viewer form, add new signatory fields, add active boolean in PO/GRSES

--- wika_dashboard/models/mail_activity.py
+++ wika_dashboard/models/mail_activity.py
@@ -3,6 +3,8 @@ from odoo import api, fields, models
 class InheritMailActivity(models.Model):
     _inherit = "mail.activity"
 
+    res_model_name = fields.Char(string='Menu', related='res_model_id.name')
+
     def action_open_document(self):
         """ Opens the related record based on the model and ID """
         self.ensure_one()
@@ -15,8 +17,15 @@ class InheritMailActivity(models.Model):
                 'view_mode': 'form',
                 'view_id': self.env.ref('wika_purchase.purchase_order_form_wika').id,
             }
-        # elif self.res_model == 'stock.picking':
-        #     BUAT_LOGIC_DISINI
+        elif self.res_model == 'stock.picking':
+            return {
+                'res_id': self.res_id,
+                'res_model': self.res_model,
+                'target': 'current',
+                'type': 'ir.actions.act_window',
+                'view_mode': 'form',
+                'view_id': self.env.ref('wika_inventory.stock_picking_form_wika').id,
+            }
         else:
             return {
                 'res_id': self.res_id,
