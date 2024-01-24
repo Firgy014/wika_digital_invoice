060bf8bffaa7e72872cd27743513488939927bd6
Susi <vsusii9@gmail.com>
2024-01-24 17:29:18 +0700

    Dev susi

--- wika_purchase/models/purchase.py
+++ wika_purchase/models/purchase.py
@@ -1,6 +1,10 @@
 from odoo import api, fields, models, _
 from datetime import datetime
 from odoo.exceptions import AccessError, ValidationError
+from odoo.exceptions import UserError
+import requests
+import json
+
 
 class PurchaseOrderInherit(models.Model):
     _inherit = 'purchase.order'
@@ -15,13 +19,13 @@ class PurchaseOrderInherit(models.Model):
         ('approved', 'Approved')
     ])
     po_type = fields.Char(string='Purchasing Doc Type')
-    begin_date = fields.Date(string='Tgl Mulai Kontrak', required=True)
-    end_date = fields.Date(string='Tgl Akhir Kontrak', required=True)
+    begin_date = fields.Date(string='Tgl Mulai Kontrak')
+    end_date = fields.Date(string='Tgl Akhir Kontrak')
     document_ids = fields.One2many('wika.po.document.line', 'purchase_id', string='Purchase Order Document Lines')
     history_approval_ids = fields.One2many('wika.po.approval.line', 'purchase_id',
                                            string='Purchase Order Approval Lines')
-    sap_doc_number = fields.Char(string='Nomor Kontrak', required=True)
-    step_approve = fields.Integer(string='Step Approve')
+    sap_doc_number = fields.Char(string='Nomor Kontrak')
+    step_approve = fields.Integer(string='Step Approve',default=1)
     picking_count = fields.Integer(string='Picking', compute='_compute_picking_count')
     kurs = fields.Float(string='Kurs')
     currency_name = fields.Char(string='Currency Name', related='currency_id.name', readonly=False)
@@ -32,22 +36,118 @@ class PurchaseOrderInherit(models.Model):
     address = fields.Char(string='Alamat')
     job = fields.Char(string='Pekerjaan')
     price_cut_ids = fields.One2many('wika.po.pricecut.line', 'purchase_id', string='Other Price Cut')
-    active = fields.Boolean(string='Active')
+    active = fields.Boolean(string='Active',default=True)
+    tgl_create_sap= fields.Date(string='Tgl Create SAP')
+
+    def get_gr(self):
+        url_config = self.env['wika.integration'].search([('name', '=', 'URL GR')], limit=1).url
+        print ("-------------------------------")
+        print (url_config)
+        headers = {
+            'Authorization': 'Basic V0lLQV9JTlQ6SW5pdGlhbDEyMw==',
+            'Content-Type': 'application/json'
+        }
+        payload = json.dumps({
+            "IV_EBELN": "%s",
+            "IW_CPUDT_RANGE": {
+                "CPUDT_LOW": "%s",
+                "CPUTM_LOW": "00:00:00",
+                "CPUDT_HIGH": "2025-12-31",
+                "CPUTM_HIGH": "23:59:59"
+            }
+        }) % (self.name, self.begin_date)
+        payload = payload.replace('\n', '')
+        print(payload)
+        try:
+            response = requests.request("GET", url_config, data=payload, headers=headers)
+            txt = json.loads(response.text)
+        except:
+            raise UserError(_("Connection Failed. Please Check Your Internet Connection."))
+        if txt['DATA']:
+            txt_data = txt['DATA']
+            vals = []
+            vals_header = []
+            for hasil in txt_data:
+                print(hasil)
+                po_exist  = self.env['purchase.order'].sudo().search([
+                    ('name', '=' ,hasil['PO_NUMBER'])] ,limit=1)
+                if po_exist:
+                    prod = self.env['product.product'].sudo().search([
+                        ('default_code', '=', hasil['MATERIAL'])], limit=1)
+                    qty = float(hasil['QUANTITY']) * 100
+                    uom = self.env['uom.uom'].sudo().search([
+                        ('name', '=', hasil['ENTRY_UOM'])], limit=1)
+                    if po_exist.po_type=='BARANG':
+                        tipe_gr='gr'
+                    else:
+                        tipe_gr = 'ses'
+                    if not uom:
+                        uom = self.env['uom.uom'].sudo().create({
+                            'name': hasil['ENTRY_UOM'], 'category_id': 1})
+                    vals.append((0, 0, {
+                        'product_id': prod.id if prod else False,
+                        'quantity_done': qty,
+                        'product_uom': uom.id,
+                        'location_id': 4,
+                        'location_dest_id': 8,
+                        'name':hasil['MAT_DOC']
+                    }))
+                #     vals_header.append((0, 0, {
+                #         'name': hasil['MAT_DOC'],
+                #         'po_id': po_exist.id,
+                #         'project_id': po_exist.project_id.id,
+                #         'branch_id': po_exist.branch_id.id,
+                #         'department_id': po_exist.department_id.id,
+                #         'scheduled_date':hasil['DOC_DATE']
+                #     }))
+                matdoc=hasil['MAT_DOC']
+                docdate = hasil['DOC_DATE']
+                gr_type=hasil['MATERIAL']
+            if vals:
+                print("ppppppppppppppppppppp")
+                picking_create = self.env['stock.picking'].sudo().create({
+                    'name': matdoc,
+                    'po_id': po_exist.id,
+                    'project_id': po_exist.project_id.id,
+                    'branch_id': po_exist.branch_id.id,
+                    'department_id': po_exist.department_id.id if po_exist.department_id.id else False,
+                    'scheduled_date':docdate,
+                    'partner_id':po_exist.partner_id.id,
+                    'location_id':4,
+                    'location_dest_id':8,
+                    'picking_type_id':1,
+                    'move_ids':vals,
+                    'pick_type':tipe_gr,
+                    #'move_ids_without_package':vals,
+                    'company_id':1,
+                    'state':'waits'
+                })
+            else:
+                raise UserError(_("Data GR Tidak Tersedia di PO TERSEBUT!"))
+
+        else:
+            raise UserError(_("Data GR Tidak Tersedia!"))
 
     @api.model_create_multi
     def create(self, vals_list):
         model_model = self.env['ir.model'].sudo()
         document_setting_model = self.env['wika.document.setting'].sudo()
         model_id = model_model.search([('model', '=', 'purchase.order')], limit=1)
-        approval_id = self.env['wika.approval.setting'].sudo().search([('model_id', '=', model_id.id)], limit=1)
-        approval_line_id = self.env['wika.approval.setting.line'].sudo().search([('approval_id', '=', approval_id.id)], limit=1)
 
-        if approval_line_id:
-            first_user = approval_line_id.groups_id.users[0]
+
 
         for vals in vals_list:
             res = super(PurchaseOrderInherit, self).create(vals)
-
+            approval_id = self.env['wika.approval.setting'].sudo().search(
+                [('model_id', '=', model_id.id), ('branch_id', '=', res.branch_id.id)], limit=1)
+            approval_line_id = self.env['wika.approval.setting.line'].search([
+                ('branch_id', '=', res.branch_id.id),
+                ('sequence', '=', 1),
+                ('approval_id', '=', approval_id.id)
+            ], limit=1)
+            print(approval_line_id)
+            if approval_line_id:
+                first_user = approval_line_id.groups_id.users[0]
             # Get Document Setting
             document_list = []
             doc_setting_id = document_setting_model.search([('model_id', '=', model_id.id)])
@@ -70,6 +170,7 @@ class PurchaseOrderInherit(models.Model):
                     'summary': f"The required documents of {model_id.name} is not uploaded yet. Please upload it immediately!"
                 })
 
+
             else:
                 raise AccessError("Either approval and/or document settings are not found. Please configure it first in the settings menu.")
 
@@ -81,7 +182,7 @@ class PurchaseOrderInherit(models.Model):
         cek = False
         model_id = self.env['ir.model'].search([('model', '=', 'purchase.order')], limit=1)
         if model_id:
-            model_wika_id = self.env['wika.approval.setting'].search([('model_id', '=', model_id.id)], limit=1)
+            model_wika_id = self.env['wika.approval.setting'].search([('model_id', '=', model_id.id),('branch_id', '=', self.branch_id.id)], limit=1)
 
         if self.branch_id and not self.department_id:
             if user.branch_id.id == self.branch_id.id and model_wika_id:
@@ -124,6 +225,13 @@ class PurchaseOrderInherit(models.Model):
         if cek == True:
             if model_wika_id.total_approve == self.step_approve:
                 self.state = 'approved'
+                self.env['wika.po.approval.line'].create({
+                    'user_id': self._uid,
+                    'groups_id': groups_id.id,
+                    'date': datetime.now(),
+                    'note': 'Approved',
+                    'purchase_id': self.id
+                })
                 folder_id = self.env['documents.folder'].sudo().search([('name', '=', 'Purchase')], limit=1)
                 if folder_id:
                     facet_id = self.env['documents.facet'].sudo().search([
@@ -148,17 +256,32 @@ class PurchaseOrderInherit(models.Model):
                             })
                 if self.activity_ids:
                     for x in self.activity_ids.filtered(lambda x: x.status == 'todo'):
-                        x.status = 'approved'
+                        if x.user_id.id==self._uid:
+                            x.status = 'approved'
             else:
                 self.step_approve += 1
-
-            self.env['wika.po.approval.line'].create({
-                'user_id': self._uid,
-                'groups_id': groups_id.id,
-                'date': datetime.now(),
-                'note': 'Approved',
-                'purchase_id': self.id
-            })
+                # Createtodoactivity
+                for x in groups_id.users:
+                    self.env['mail.activity'].create({
+                        'activity_type_id': 4,
+                        'res_model_id': self.env['ir.model'].sudo().search([('model', '=', 'purchase.order')],
+                                                                           limit=1).id,
+                        'res_id': self.id,
+                        'user_id': x.id,
+                        'summary': """Need Approval Document PO"""
+                    })
+                self.env['wika.po.approval.line'].create({
+                    'user_id': self._uid,
+                    'groups_id': groups_id.id,
+                    'date': datetime.now(),
+                    'note': 'Approved',
+                    'purchase_id': self.id
+                })
+                if self.activity_ids:
+                    for x in self.activity_ids.filtered(lambda x: x.status == 'todo'):
+                        if x.user_id.id==self._uid:
+                            print ("llllllllllll")
+                            x.status = 'approved'
 
         else:
             raise ValidationError('User Akses Anda tidak berhak Approve!')
@@ -202,18 +325,25 @@ class PurchaseOrderInherit(models.Model):
                 for doc_line in self.document_ids:
                     if doc_line.document != False:
                         model_id = self.env['ir.model'].search([('model', '=', 'purchase.order')], limit=1)
-                        model_wika_id = self.env['wika.approval.setting'].search([('model_id', '=', model_id.id)], limit=1)
+                        model_wika_id = self.env['wika.approval.setting'].search([('model_id', '=', model_id.id),('branch_id', '=', self.branch_id.id)], limit=1)
                         user = self.env['res.users'].search([('branch_id', '=', self.branch_id.id)])
-
+                        if self.activity_ids:
+                            for x in self.activity_ids.filtered(lambda x: x.status == 'todo'):
+                                print ("masuk")
+                                print (x.user_id)
+                                if x.user_id.id == self._uid:
+                                    print (x.status)
+                                    x.status = 'approved'
                         if model_wika_id:
                             self.state = 'uploaded'
                             self.step_approve += 1
-
+                            print(self.step_approve)
                             groups_line = self.env['wika.approval.setting.line'].search([
                                 ('branch_id', '=', self.branch_id.id),
                                 ('sequence', '=', self.step_approve),
                                 ('approval_id', '=', model_wika_id.id)
                             ], limit=1)
+                            print("groups",groups_line)
                             groups_id = groups_line.groups_id
 
                             for x in groups_id.users:
@@ -224,6 +354,13 @@ class PurchaseOrderInherit(models.Model):
                                     'user_id': x.id,
                                     'summary': """Need Approval Document PO"""
                                 })
+                            self.env['wika.po.approval.line'].create({
+                                'user_id': self._uid,
+                                'groups_id': groups_id.id,
+                                'date': datetime.now(),
+                                'note': 'Submit Document',
+                                'purchase_id': self.id
+                            })
                         else:
                             raise ValidationError('Approval Setting untuk menu Purchase Orders tidak ditemukan. Silakan hubungi Administrator!')
 
@@ -235,14 +372,15 @@ class PurchaseOrderInherit(models.Model):
 
     def get_picking(self):
         self.ensure_one()
-        view_id = self.env.ref("wika_inventory.stock_picking_tree_wika", raise_if_not_found=False)
-        
+        view_tree_id = self.env.ref("wika_inventory.stock_picking_tree_wika", raise_if_not_found=False)
+        view_form_id = self.env.ref("wika_inventory.stock_picking_form_wika", raise_if_not_found=False)
+
         return {
             'name': _('GR/SES'),
             'type': 'ir.actions.act_window',
-            'view_mode': 'tree',
+            'view_mode': 'tree,form',
             'res_model': 'stock.picking',
-            'view_id': view_id.id,
+            'view_ids': [(view_tree_id, 'tree'), (view_form_id, 'form')],
             'res_id': self.id,
             'domain': [('po_id', '=', self.id)],  
             'context': {'default_po_id': self.id},
