d5e3e6520ede5306ab3dc2dc6f5642b774b7a86a
firgysciglio@gmail.com <firgysciglio@gmail.com>
2024-01-24 17:14:41 +0700

    create onchange product in wika_berita_acara_pembayaran & update values report report_wika_berita_acara_pembayaran

--- wika_berita_acara_pembayaran/models/wika_outstanding_bap.py
+++ wika_berita_acara_pembayaran/models/wika_outstanding_bap.py
@@ -1,9 +1,11 @@
 from odoo import models, fields, api
 from odoo.exceptions import UserError, ValidationError, Warning, AccessError
+from odoo import tools
 
 class WikaOutstandingBap(models.Model):
     _name = 'wika.outstanding.bap'
     _description = 'Wika Outstanding BAP'
+    _auto = False
 
     purchase_id = fields.Many2one('purchase.order', string='Nomor PO')
     project_id = fields.Many2one('project.project', string='Project')
@@ -19,10 +21,13 @@ class WikaOutstandingBap(models.Model):
     qty_process = fields.Integer(string='Quantity Proses')
     no_bap = fields.Char(string='Nomor BAP')
     
-    @api.model
-    def init(self):
-        cr = self.env.cr
-        cr.execute("SELECT * FROM purchase_order WHERE active = true")
-        result = cr.fetchone()
-        print("BERHASILLLLLLLLLLLLLLLL")
-        print("BERHASILLLLLLLLLLLLLLLL", result)
+    # @api.model_cr
+    # def init(self):
+    #     tools.drop_view_if_exists(self._cr, 'wika_berita_acara_pembayaran')
+    #     self._cr.execute("""CREATE or REPLACE VIEW wika_outstanding_bap as (
+    #         select id as purchase_id from purchase_order)""")
+    #     # cr = self.env.cr
+    #     # cr.execute("SELECT * FROM purchase_order WHERE active = true")
+    #     # result = cr.fetchone()
+    #     # print("BERHASILLLLLLLLLLLLLLLL")
+        # print("BERHASILLLLLLLLLLLLLLLL", result)
