287a4e38f58c9cd47bd2e61655bc56f9119dc4ee
Susi <vsusii9@gmail.com>
2024-02-19 00:48:50 +0700

    FIX form Invoice

--- wika_inventory/models/stock_move.py
+++ wika_inventory/models/stock_move.py
@@ -11,9 +11,42 @@ class StockMoveInherit(models.Model):
         ('rejected', 'Rejected')
 
     ], string='Status', default='waits')
-    qty_bap=fields.Float(string='Qty BAP')
+    qty_bap = fields.Float('Total BAP', compute='_compute_qty_bap')
+    sisa_qty_bap = fields.Float('Sisa Qty BAP', compute='_compute_sisa_qty_bap')
 
 
+    def _compute_qty_bap(self):
+        for x in self:
+            query = """select sum(qty) from wika_berita_acara_pembayaran_line where stock_move_id=%s
+            """% (x.id)
+            print (query)
+            self.env.cr.execute(query)
+            result = self.env.cr.fetchone()
+            # bap_line_model = self.env['wika.berita.acara.pembayaran.line'].sudo()
+            #
+            # total_qty = 0
+            # bap_line_ids = bap_line_model.search([('stock_move_id', '=', self.id)]).qty
+            # bap_lines = bap_line_model.browse(bap_line_ids)
+            #
+            # for bap_line in bap_lines:
+            #     total_qty += bap_line.qty
+            if result:
+                x.qty_bap = result[0]
+            else:
+                x.qty_bap=0.0
+
+    @api.depends('qty_bap','quantity_done')
+    def _compute_sisa_qty_bap(self):
+        for x in self:
+            if x.qty_bap>0:
+                x.sisa_qty_bap = x.quantity_done - x.qty_bap
+            else:
+                x.sisa_qty_bap= x.quantity_done
+        # bap_line_model = self.env['wika.berita.acara.pembayaran.line'].sudo()
+        # bap_line_ids = bap_line_model.search([('picking_id', '=', self.picking_id.id)])
+        # if bap_line_ids:
+        #     for bap_line in bap_line_ids:
+        #         self.sisa_qty_bap = bap_line.qty - self.qty_bap
 
     # @api.model_create_multi
     # def create(self, vals_list):
@@ -29,11 +62,5 @@ class StockMoveInherit(models.Model):
     
     @api.depends('quantity_done', 'price_unit')
     def _compute_price_subtotal(self):
-        print('masuk.def.compute.stokmuv')
         for record in self:
-            print("qtydon", record.quantity_done)
-            print("qtydon", record.quantity_done)
-            print("prcunit", record.price_unit)
-            # vardump
-            print('masuk.for.compute.stokmuv')
             record.price_subtotal = record.quantity_done * record.price_unit
