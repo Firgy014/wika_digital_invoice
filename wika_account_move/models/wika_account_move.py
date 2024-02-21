8da92ed627254ef56e4697fed58732d6b2d3f2a0
Susi <vsusii9@gmail.com>
2024-02-21 18:07:14 +0700

    Fix ACCOUNT_MOVE

--- wika_account_move/models/wika_account_move.py
+++ wika_account_move/models/wika_account_move.py
@@ -32,15 +32,20 @@ class WikaInheritedAccountMove(models.Model):
     check_biro = fields.Boolean(compute="_cek_biro")
 
     pph_ids = fields.Many2many('account.tax', string='PPH')
-    total_pph = fields.Monetary(string='Total PPH', readonly=False, compute='')
+    total_pph = fields.Monetary(string='Total PPH', readonly=False, compute='_compute_total_pph')
 
     price_cut_ids = fields.One2many('wika.account.move.pricecut.line', 'move_id', string='Other Price Cut')
     account_id = fields.Many2one(comodel_name='account.account')
     date = fields.Date(string='Posting Date', default=lambda self: fields.Date.today())
 
-    @api.onchange('pph_ids')
-    def _onchange_pph_ids(self):
-        pass
+    @api.depends('tax_totals', 'pph_ids.amount')
+    def _compute_total_pph(self):
+        for record in self:
+            print(record.tax_totals)
+            total_pph = 0.0
+            for pph in record.pph_ids:
+                total_pph += (record.tax_totals['amount_untaxed'] * pph.amount) / 100
+            record.total_pph = total_pph
     
     @api.depends('department_id')
     def _cek_biro(self):
@@ -80,15 +85,14 @@ class WikaInheritedAccountMove(models.Model):
 
     @api.onchange('date')
     def _onchange_posting_date(self):
+        if not self.date or not self.bap_id or not self.bap_id.bap_date:
+            return
         if isinstance(self, bool):
             return self
         if len(self) != 1:
             raise ValidationError("Hanya satu record yang diharapkan diperbarui!")
-
-        if self.date != False and self.date < self.bap_id.bap_date:
+        if self.date < self.bap_id.bap_date:
             raise ValidationError("Posting Date harus lebih atau sama dengan Tanggal BAP yang dipilih!")
-        else:
-            pass
 
     # invoice_line_ids = fields.One2many(  # /!\ invoice_line_ids is just a subset of line_ids.
     #     'account.move.line',
