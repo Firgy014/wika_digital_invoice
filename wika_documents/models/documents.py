2ac2b461200a8f67acf5160697036c994c1587e1
Susi <vsusii9@gmail.com>
2024-02-21 23:55:45 +0700

    Fix Warning Amount Invoice

--- wika_documents/models/documents.py
+++ wika_documents/models/documents.py
@@ -7,3 +7,4 @@ class DocumentsDocumentInherit(models.Model):
     picking_id = fields.Many2one('stock.picking', string='Nomor GR/SES')
     is_po_doc = fields.Boolean(string='Is Purchase Orders Document')
     invoice_id = fields.Many2one('account.move', string='No Invoice')
+    bap_id = fields.Many2one('wika.berita.acara.pembayaran', string='No BAP')
