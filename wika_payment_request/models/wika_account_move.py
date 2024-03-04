9c67e5fa9d7bbfb2d8613ca65e958db231643f8b
Susi <vsusii9@gmail.com>
2024-03-04 11:01:28 +0700

    Field Payment status

--- wika_payment_request/models/wika_account_move.py
+++ wika_payment_request/models/wika_account_move.py
@@ -10,6 +10,14 @@ class InheritAccountMove(models.Model):
     total_partial_pr=fields.Float(string='Total Partial Payment Request',compute='_compute_amount_pr')
     amount_sisa_pr=fields.Float(string='Sisa Partial Payment Request',compute='_compute_amount_pr')
     is_partial_pr=fields.Boolean(string='Partial Payment Request',default=False,compute='_compute_is_partial_pr')
+    status_payment = fields.Selection([
+        ('Not Request', 'Not Request'),
+        ('Request Proyek', 'Request Proyek'),
+        ('Request Divisi', 'Request Divisi'),
+        ('Request Pusat', 'Request Pusat'),
+        ('Ready To Paid', 'Ready To Paid'),
+        ('Paid', 'Paid')
+    ], string='Payment State',default='Not Request')
 
     @api.depends('partial_request_ids','total_line')
     def _compute_amount_pr(self):
@@ -24,7 +32,7 @@ class InheritAccountMove(models.Model):
     @api.depends('total_partial_pr')
     def _compute_is_partial_pr(self):
         for x in self:
-            if x.total_partial_pr>0:
+            if x.partial_request_ids:
                 x.is_partial_pr =True
             else:
                 x.is_partial_pr=False
