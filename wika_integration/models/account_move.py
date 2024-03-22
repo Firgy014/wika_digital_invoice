d7e71c5db70e4fffc74d8cde4e232e0f7d3ea540
andromedasupendi <andromedasupendi@gmail.com>
2024-03-22 09:53:05 +0700

    add new API to update payment_reference in invoice from SAP

--- wika_integration/models/account_move.py
+++ wika_integration/models/account_move.py
@@ -4,3 +4,4 @@ class AccountMoveInheritWika(models.Model):
     _inherit = 'account.move'
     
     is_generated = fields.Boolean(string='Generated to TXT File', default=False)
+    year = fields.Char(string='Invoice Year')
