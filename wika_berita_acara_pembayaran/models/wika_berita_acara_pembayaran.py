901826218307e4ead2f06c9a8c6157dad5f342af
Susi <vsusii9@gmail.com>
2024-04-24 12:29:00 +0700

    add validation BAP date cannot be other than the current month and year

--- wika_berita_acara_pembayaran/models/wika_berita_acara_pembayaran.py
+++ wika_berita_acara_pembayaran/models/wika_berita_acara_pembayaran.py
@@ -45,7 +45,7 @@ class WikaBeritaAcaraPembayaran(models.Model):
         auto_join=True,
         groups="base.group_user",)
     end_date = fields.Date(string='Tgl Akhir Kontrak', related="po_id.end_date")
-    bap_date = fields.Date(string='Tanggal BAP', required=True)
+    bap_date = fields.Date(string='Tanggal BAP', required=True,default=datetime.today())
     bap_type = fields.Selection([
         ('progress', 'Progress'),
         ('uang muka', 'Uang Muka'),
@@ -143,6 +143,28 @@ class WikaBeritaAcaraPembayaran(models.Model):
     total_adjustment = fields.Float('Total Adjustment', compute='_compute_total_adjustment')
     total_po = fields.Float(string='Total PO', compute='_compute_total_po')
     remain_val_po = fields.Float(string='Sisa BAP')
+
+    @api.onchange('bap_date')
+    def _onchange_bap_date(self):
+        if self.bap_date:
+            chosen_date = fields.Date.from_string(self.bap_date)
+            current_date = datetime.now().date()
+            if chosen_date.year != current_date.year or chosen_date.month != current_date.month:
+                return {
+                    'warning': {
+                        'title': "Peringatan!",
+                        'message': "Anda hanya dapat memilih tanggal BAP pada bulan dan tahun ini."
+                    }
+                }
+
+    @api.constrains('bap_date')
+    def _check_bap_date(self):
+        for record in self:
+            if record.bap_date:
+                chosen_date = fields.Date.from_string(record.bap_date)
+                current_date = datetime.now().date()
+                if chosen_date.year != current_date.year or chosen_date.month != current_date.month:
+                    raise ValidationError("Anda hanya dapat memilih tanggal BAP pada bulan dan tahun ini.")
 
     @api.onchange('partner_id', 'bap_ids.product_id')
     def _onchange_partner_id_product_id(self):
