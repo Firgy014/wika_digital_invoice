52040fbf8556a55e9fe4c8d676840091e6a1ee02
Kikin Kusumah <kinsoft.indonesia@gmail.com>
2024-07-10 15:26:27 +0700

    memindahkan field2 dari wika_integration ke wika_account_move

--- wika_integration/models/account_move.py
+++ wika_integration/models/account_move.py
@@ -5,53 +5,53 @@ _logger = logging.getLogger(__name__)
 class AccountMoveInheritWika(models.Model):
     _inherit = 'account.move'
     
-    is_generated = fields.Boolean(string='Generated to TXT File', default=False)
-    year = fields.Char(string='Invoice Year')
-    dp_doc = fields.Char(string='DP Doc')
-    retensi_doc = fields.Char(string='Retensi Doc')
-    sap_amount_payment = fields.Float('Amount Payment', tracking=True)
-    amount_due = fields.Float('Amount Due', compute='_compute_amount_due')
-    amount_idr = fields.Float(string='Amount IDR', store=True)
+    # is_generated = fields.Boolean(string='Generated to TXT File', default=False)
+    # year = fields.Char(string='Invoice Year')
+    # dp_doc = fields.Char(string='DP Doc')
+    # retensi_doc = fields.Char(string='Retensi Doc')
+    # sap_amount_payment = fields.Float('Amount Payment', tracking=True)
+    # amount_due = fields.Float('Amount Due', compute='_compute_amount_due')
+    # amount_idr = fields.Float(string='Amount IDR', store=True)
 
-    def _compute_amount_due(self):
-        _logger.info("# === _compute_amount_due === #")
-        for rec in self:
-            total_paid = 0
-            if rec.partial_request_ids:
-                tot_partial_amount = sum(rec.partial_request_ids.filtered(lambda x : x.payment_state == 'paid').mapped('partial_amount'))
-                residual_amount = rec.total_line - tot_partial_amount
-                # residual_amount = rec.sisa_partial
-            else:    
-                total_paid = rec.sap_amount_payment
-                residual_amount = rec.amount_total_footer - total_paid
+    # def _compute_amount_due(self):
+    #     _logger.info("# === _compute_amount_due === #")
+    #     for rec in self:
+    #         total_paid = 0
+    #         if rec.partial_request_ids:
+    #             tot_partial_amount = sum(rec.partial_request_ids.filtered(lambda x : x.payment_state == 'paid').mapped('partial_amount'))
+    #             residual_amount = rec.total_line - tot_partial_amount
+    #             # residual_amount = rec.sisa_partial
+    #         else:    
+    #             total_paid = rec.sap_amount_payment
+    #             residual_amount = rec.amount_total_footer - total_paid
             
-            _logger.info("Total Paid %s Residual Amount %s" % (str(total_paid), str(residual_amount)))
+    #         _logger.info("Total Paid %s Residual Amount %s" % (str(total_paid), str(residual_amount)))
 
-            rec.amount_due = residual_amount
+    #         rec.amount_due = residual_amount
     
-    def _compute_status_payment(self):
-        for rec in self:
-            rec._compute_amount_due()
-            if rec.state != 'draft':
-                if rec.amount_due <= 0:
-                    rec.status_payment = 'Paid'
-                else:
-                    rec.status_payment = 'Not Request'
-            else:
-                rec.status_payment = 'Not Request'
+    # def _compute_status_payment(self):
+    #     for rec in self:
+    #         rec._compute_amount_due()
+    #         if rec.state != 'draft':
+    #             if rec.amount_due <= 0:
+    #                 rec.status_payment = 'Paid'
+    #             else:
+    #                 rec.status_payment = 'Not Request'
+    #         else:
+    #             rec.status_payment = 'Not Request'
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    pph_cash_basis = fields.Float(
-        string='PPh Cash Basis',
-        readonly=False,
-        digits='Product Price',
-    )
+    # pph_cash_basis = fields.Float(
+    #     string='PPh Cash Basis',
+    #     readonly=False,
+    #     digits='Product Price',
+    # )
 
-    @api.constrains('account_id', 'display_type')
-    def _check_payable_receivable(self):
-        return
+    # @api.constrains('account_id', 'display_type')
+    # def _check_payable_receivable(self):
+    #     return
         #override
         # for line in self:
             # account_type = line.account_id.account_type
